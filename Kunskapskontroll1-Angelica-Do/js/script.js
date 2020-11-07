c7f9d572d2c2440e948528ed5d445c534cb7edb2
Angelica <do.angelica@gmail.com>
2020-11-07 14:27:28 +0100

    Done

--- Kunskapskontroll1-Angelica-Do/js/script.js
+++ Kunskapskontroll1-Angelica-Do/js/script.js
@@ -5,49 +5,85 @@ resetBtn.innerText = 'RESET';
 // EVENT LISTENER - RESET BTN
 resetBtn.addEventListener('click', resetAll);
 
-// Change color when click on Article 3 Paragraph 
+
+//EVENTLISTENERS 
+
+// Select and add EventListener - click on Article 3 Paragraph 
 let coloredP = document.querySelector('.art-3 p');
 coloredP.addEventListener('click', function (events){
 colorPTag();
 });
 
-// Change background color on an element
+// Select and add EventListener on Art-1 background
 let backgroundArt1 = document.querySelector('.art-1');
 backgroundArt1.addEventListener('mouseenter', function (events){
 changeBackgroundColor();
 });
 
-// Change one photo
+// Select and add EventListener on Art-2 Photo
 let art2Photo = document.querySelector('.art-2 img');
 art2Photo.addEventListener('click', function(event){
 changePhoto();
 });
 
-//Change text on one element 
+//Select and add EventListener on Art2 Paragraph
 let art2p = document.querySelector('.art-2 p');
 art2p.addEventListener('click', function(event){
   changeText();
 });
 
-//Change color on button
+//Select and add EventListener on button two
 let btns = document.querySelectorAll('button');
 let colorBtn = btns[2];
 colorBtn.addEventListener('mouseenter',function(event){
 changeColorBtn();
 });
 
-// Remove one element
-let nav = document.querySelector('#header-navigation');
-nav.addEventListener('click', function(event){
+// Select and add EventListener on Nav 
+let logo = document.querySelector('.logo-random-class');
+logo.addEventListener('click', function(event){
 removeMenu();
 });
-// Add a list with several Li-elements with border 
 
+// Select Cart and add event 
+let cart = document.querySelector('.cart');
+cart.addEventListener('click', function(alert){
+alertCart();
+});
 
+// Create ul with Li-elements with border 
+let list = document.querySelector('.footer');
+list.insertAdjacentHTML('afterend','<ul>My List</ul>');
 
+let listItem = document.createElement('li');
+let listItemName = document.createTextNode('Item 1');
+listItem.appendChild(listItemName);
+document.querySelector('ul').appendChild(listItem);
 
+let listItem2 = document.createElement('li');
+let listItemName2 = document.createTextNode('Item 2');
+listItem2.appendChild(listItemName2);
+document.querySelector('ul').appendChild(listItem2);
 
+let listItem3 = document.createElement('li');
+let listItemName3 = document.createTextNode('Item 3');
+listItem3.appendChild(listItemName3);
+document.querySelector('ul').appendChild(listItem3);
 
+//Style Ul/Li-elements 
+let borderLi = document.querySelectorAll('li');
+borderLi[0].style.border = '4px solid black';
+borderLi[1].style.border = '4px solid red';
+borderLi[2].style.border = '4px solid blue';
+
+let borderUl = document.querySelector('ul');
+borderUl.style.border = '6px solid purple';
+
+// Select and add event on a 
+let navigationA = document.querySelector('nav a');
+navigationA.addEventListener('click',function(event){
+strongNav();
+});
 
 // RESET FUNCTION 
 function resetAll() {
@@ -69,10 +105,17 @@ oldText.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mi
 // Reset art 3 button
 let oldBtn = document.querySelector('.art-3 button');
 oldBtn.style.backgroundColor ='black';
-// Reset menu
-let addMenu = document.querySelector('.footer a' );
-addMenu.insertAdjacentHTML('beforebegin', '<h3>Menu</h3>');            
+// Reset menu - Kommer plotta ut H3 vid varje klick på RESET 
+let addMenu= document.querySelector('.footer a');
+addMenu.insertAdjacentHTML('beforebegin', ' <h3>Menu</h3>');
+
+// Remove UL 
+let removeUl = document.querySelector('ul');
+removeUl.remove();
 }
+
+
+
 /* FUNCTIONS */
 // 1. Change color when click on Article 3's Paragraph 
 function colorPTag(){
@@ -103,9 +146,19 @@ function changeColorBtn (){
 coloredBtn.style.backgroundColor = 'red';
 }
 
-// 6. Remove menu- h3 in footer
+// 6. Remove menu heading in the footer
 function removeMenu() {
     let menu = document.querySelector('.footer h3');
     menu.remove();
 }
-
+//7. Alert on cart
+function alertCart(){
+    let alertonCart = document.querySelector('.cart');
+   alert('Hey, you are shopping to much!');
+} 
+// Underline Home in Nav
+function strongNav(){
+    let nav = document.querySelectorAll('nav a');
+    let a = nav[0];
+   a.style.textDecoration = 'underline';
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kunskapskontroll1-Angelica-Do
-JS - kunskapskontroll 1
+Som ux-designer försökte jag mig på att skapa en högre användbarhet för sidan, genom bland annat:
+1. Se var man är på sidan genom underline på "Home" (tryck på Home).
+2. Tyck på Varukorg-ikonen - en alert dyker upp.
+3. Tryck på Texten på Art-2 - Ändrar beskrivning
+4. RESET knapp tar bort alla ändringar. Dock så kommer den att lägga till "Menu" i footern vid fler tryck efter varandra.
+
+
+Ändringar jag inte kan stå bakom som ux-designer men experimenterade med: 
+1. Tryck på Sinus-loggan - Menu H3 i footern försvinner
+2. Hover över Art-1 - bakgrunden blir blå
+3. Tryck på röd hoodie - Den blir grön
+4. Tryck på Text på Art-3 - texten blir röd
+5. Hover över knapp på Art-3 - den blir röd
+6. Där är en lista i footern. 
